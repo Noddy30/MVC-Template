a5d9b490030490ea8f8ed381c4454359923a6c59
Noddy30 <neduan41@gmail.com>
2023-09-22 21:42:27 +0200

    ScoreCard Index

---
+++ Template/Controllers/ScoreCardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Template.Repositories.ScoreCards;
+
+namespace Template.Controllers
+{
+	public class ScoreCardController : Controller
+    {
+        private readonly ILogger<ScoreCardController> _logger;
+        private readonly IScoreCardRepository _scoreCardRepository;
+
+        public ScoreCardController(ILogger<ScoreCardController> logger,
+            IScoreCardRepository scoreCardRepository)
+		{
+            _logger = logger;
+            _scoreCardRepository = scoreCardRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Paginate()
+        {
+            var data = await _scoreCardRepository.GetAllScoreCardsAsync();
+            return Json(data);
+        }
+    }
+}
+
--- Template/Program.cs
+++ Template/Program.cs
@@ -7,6 +7,7 @@ using Template.Areas.Identity.Data;
 using Template.Data;
 using Template.Repositories.Courses;
 using Template.Repositories.Players;
+using Template.Repositories.ScoreCards;
 using Template.Repositories.Users;
 using Template.Services;
 
@@ -32,6 +33,7 @@ namespace Template
             builder.Services.AddScoped<IUserRepository, UserRepository>();
             builder.Services.AddTransient<IEmailService, EmailService>();
             builder.Services.AddTransient<ICourseRepository, CourseRepository>();
+            builder.Services.AddTransient<IScoreCardRepository, ScoreCardRepository>();
 
 
             builder.Services.AddDbContext<AppDbContext>(options =>
